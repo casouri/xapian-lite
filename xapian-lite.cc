445df793dc51ba0d2122aa3d2e5274eae1224f62
Yuan Fu <casouri@gmail.com>
2023-01-10 01:48:46 -0800

    ; * xapian-lite.cc: Add copyright notice.

--- xapian-lite.cc
+++ xapian-lite.cc
@@ -1,3 +1,24 @@
+/* Xapian-lite.
+
+Copyright (C) 2021-2023 Free Software Foundation, Inc.
+
+Maintainer: Yuan Fu <casouri@gmail.com>
+
+This file is part of GNU Emacs.
+
+GNU Emacs is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+GNU Emacs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
+
 #include <string>
 #include <cstring>
 #include <iostream>
